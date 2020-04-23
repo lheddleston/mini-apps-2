b8f6bad18560265c3ab0f84bebdaddc29c0a670f
Laura <laura.heddleston@gmail.com>
2020-04-23 13:47:00 -0700

    Initial Commit - setup

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+var path = require('path');
 
+/********* Middleware *********/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/********* Start App *********/
+app.use(express.static(path.join(__dirname, './public/')));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+})
 
---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/');
+var DIST_DIR = path.join(__dirname, '/public/');
+
+module.exports = {
+  entry: `${SRC_DIR}/app.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        options: {
+          'presets': ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+};
